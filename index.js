a68a8eddd18c9f87013b291f5a2b930c3e89a784
JVisi <justvisitf@gmail.com>
2020-04-04 21:14:16 +0200

    models added, waifu/harem missing yet

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express=require('express');
 const cors=require('cors');
+const squer=require('./lib/ORM/setting').sequelize;
+
+
+const {Activity}=require('./lib/ORM/Models/activity');
+Activity.findAll().then((result)=>{
+    console.log(result[0].dataValues);
+});
 
 const port= process.env.port || 3000;
 const app=express();
