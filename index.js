ae4abc6e6cb15860b23ca533606feb9c414c3160
JVisi <justvisitf@gmail.com>
2020-08-11 21:50:18 +0200

    updates

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ tokenshit("asd");
 const port= process.env.port || 3000;
 const app=express();
 
-const {sequelize}=require('./lib/ORM/setting');
+/*const {sequelize}=require('./lib/ORM/setting');
 const {Activity}=require('./lib/ORM/Models/activity');
 const {Weapon}=require('./lib/ORM/Models/weapon');
 const {Item}=require('./lib/ORM/Models/item');
 const {Harem_Skill}=require('./lib/ORM/Models/harem_skill');
 const {SpecialSkill}=require('./lib/ORM/Models/specialSkill');
-sequelize.sync({alter:true});
+sequelize.sync({alter:true});*/
 
 app.use(bodyparser.json())
 
