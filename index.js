f4a51273c24d6a31ea8c7244bfc336ed58f313c7
JVisi <justvisitf@gmail.com>
2021-05-04 09:34:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,6 @@ const express=require('express');
 const bodyparser=require('body-parser');
 const {router}=require('./lib/router');
 
-/*
-const config=require('./lib/config');
-async function tokenshit(heli){
-    let token=await config.signToken(heli);
-    config.verifyTokenAsync(token).then((value)=>{
-        console.log("helo");
-    },
-    (rejectValue)=>{
-        console.log("nem helo");
-    });
-
-b349bdc1-c752-4520-cbb2-9c1f77g44f7c
-c144bdb2-c752-4520-cbc1-9c1f77g64f7b waifu id
-
-
-}
-tokenshit("asd");
-*/
 const port= process.env.port || 3000;
 const app=express();
 
