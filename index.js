d64b77232c58abdf7ed7ccc19a70731c0ed8329e
Patrik Visi <justvisitf@gmail.com>
2021-08-07 09:25:55 +0200

    jwt token, bugfixes

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express=require('express');
-const bodyparser=require('body-parser');
-const {router}=require('./lib/router');
+const express = require('express');
+const {json} =require ('body-parser');
+const cors =require ("cors");
+const {router} =require ('./lib/router.js');
+const cookieParser=require('cookie-parser')
 
-const port= process.env.port || 3000;
+const port= process.env.port || 5000;
 const app=express();
 
-const {sequelize}=require('./lib/ORM/setting');
-const {Activity}=require('./lib/ORM/Models/activity');
-const {Weapon}=require('./lib/ORM/Models/weapon');
-const {Item}=require('./lib/ORM/Models/item');
-const {Harem_Skill}=require('./lib/ORM/Models/harem_skill');
-const {SpecialSkill}=require('./lib/ORM/Models/specialSkill');
-sequelize.sync({alter:true});
+/* const { sequelize } require './lib/ORM/setting';
+const { Activity } require './lib/ORM/Models/activity';
+const { Weapon } require './lib/ORM/Models/weapon';
+const { Item } require './lib/ORM/Models/item';
+const { Harem_Skill } require './lib/ORM/Models/harem_skill';
+const { SpecialSkill } require './lib/ORM/Models/specialSkill'; */
+//sequelize.sync({alter:true});
 
-app.use(bodyparser.json())
-
+//app.use(_json())
+app.use(cookieParser())
+app.use(json())
+app.use(cors({credentials:true,origin:'http://localhost:3000'}))
 app.use(router);
 app.listen(port,()=>{
     console.info(`app listen on ${port}`);
