881b4adba295203d741ebcb1a96df31fe60ad1cb
Patrik Visi <justvisitf@gmail.com>
2021-08-10 15:32:16 +0200

    Images base64 encoding on the fly

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {json} =require ('body-parser');
 const cors =require ("cors");
 const {router} =require ('./lib/router.js');
-const cookieParser=require('cookie-parser')
+const cookieParser=require('cookie-parser');
 
 const port= process.env.port || 5000;
 const app=express();
@@ -13,6 +13,8 @@ const { Weapon } require './lib/ORM/Models/weapon';
 const { Item } require './lib/ORM/Models/item';
 const { Harem_Skill } require './lib/ORM/Models/harem_skill';
 const { SpecialSkill } require './lib/ORM/Models/specialSkill'; */
+
+//const { sequelize } = require('./lib/ORM/setting.js');
 //sequelize.sync({alter:true});
 
 //app.use(_json())
